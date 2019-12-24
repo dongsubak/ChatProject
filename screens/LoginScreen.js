c0e0e54d910139759b02b78fe79f5322038cf3b6
dongsubak <likeap@catholic.ac.kr>
2019-12-24 18:44:28 +0900

    LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,41 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from "react-native";
+import { Ionicons } from '@expo/vector-icons'
 
 export default class ChatScreen extends React.Component {
+    state = {
+        name: ""
+    }
+
+    continue = () => {
+        this.props.navigation.navigate("Chat", {name: this.state.name})
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Text>Screen</Text>
+                <View style={styles.circle} /> 
+                <View style={{ marginTop: 64 }}>
+                    <Image
+                        source={require("../assets/chat.png")}
+                        style={{ width: 100, height: 100, alignSelf: "center" }}
+                    />                    
+                </View>           
+                <View style={{marginHorizontal: 32 }}>
+                    <Text style={styles.header}>Username</Text>
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="DSP" 
+                        onChangeText={name => { 
+                            this.setState({ name });
+                        }} 
+                        value={this.state.name}
+                    />
+                    <View style={{alignItems: "flex-end", marginTop: 64}}>
+                        <TouchableOpacity style={styles.continue} onPress={this.continue}>
+                            <Ionicons name="md-arrow-round-forward" size={24} color="#FFF" />
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         );
     }
@@ -14,8 +44,32 @@ export default class ChatScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1, 
-        justifyContent: "center",
-        alignItems: "center"
+        backgroundColor: "#F4F5F7"
+    },
+    circle: {
+        width: 500,
+        height: 500,
+        borderRadius: 500/2,
+        backgroundColor: "#FFF",
+        position: "absolute",
+        left: -120,
+        top: -20   
+    },
+    header: {
+        fontWeight: "800",
+        fontSize: 30,
+        color: "#514E5A",
+        marginTop: 32
+    },
+    input: {
+        marginTop: 32,
+        height: 50,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: "#BAB7C3",
+        borderRadius: 30,
+        paddingHorizontal: 16,
+        color: "#514E5A",
+        fontWeight: "600"
     }
-})
+});
 
