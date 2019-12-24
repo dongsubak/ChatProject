ae956985c00b84206ad72b79237ba8dc45388296
dongsubak <likeap@catholic.ac.kr>
2019-12-24 18:56:39 +0900

    Fire.js checkAuth send parse getdb

--- Fire.js
+++ Fire.js
@@ -9,8 +9,44 @@ class Fire {
     init = () => {
         if(!firebase.apps.length) {
             firebase.initializeApp({
-                
+
             })
         }
+    }
+
+    checkAuth = () =>{
+        firebase.auth().onAuthStateChanged(user => {
+            if(!suer) {
+                firebase.auth().signInAnonymously();
+            }
+        });
+    };
+
+    send = messages => {
+        messages.forEach(item => {
+            const message = {
+                text: item.text,
+                timestamp: firebase.database.ServerValue.TIMESTAMP,
+                user: item.user
+            };
+
+            this.db.push(message);
+        });
+    };
+
+    parse = message => {
+        const {user, text, timestamp} = message.val()
+        const {Key: _id} = message
+        const createdAt = new Date(timestamp)
+
+        return {
+            _id,
+            createdAt,
+            text,
+            user
+        }
+    }
+    get db() {
+        return firebase.database().ref("messages")
     }
 }
